045c334c4ba18f728408422633019397df3d8af3
Jordan Heskett <jordanheskett@gmail.com>
2020-02-17 19:20:01 -0600

    Fixing :beetle: bugs in seeder algorithm...

--- database/seeder9000/datacreator.js
+++ database/seeder9000/datacreator.js
@@ -1,68 +1,194 @@
+const faker = require('faker');
 const fs = require('fs');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const fsPromises = fs.promises;
 
+let startTime = process.hrtime();
 
-startTime = process.hrtime();
-let paragraph = "Ex ea enim nostrum et officia. Eius non magnam rerum. Et aut accusantium possimus ex officiis voluptas molestiae non Assumenda laudantium quisquam architecto commodi vel nesciunt error ducimus quidem. Deleniti pariatur explicabo in repellendus reprehenderit et. Quidem tempore atque Consequatur exercitationem tempora voluptas quia consequatur voluptatem aut quos velit maiores perferendis aut et sint velit ut voluptatibus voluptatem quisquam voluptatem inventore molestiae dignissimos error impedit et ad asperiores ullam corrupti inventore quas nihil cupiditate fuga rerum quo ipsam illum praesentium quis asperiores et accusamus ut dolorum aut et et aliquid libero laborum nisi qui qui cum enim et illum dolorum consectetur qui totam ut illo qui cupiditate porro laudantium iusto itaque deserunt est autem eius incidunt labore reprehenderit placeat veritatis non eos et quibusdam aliquid voluptate expedita officia modi pariatur dolor quo facilis et ea ut voluptatem est cum distinctio deserunt non non omnis ducimus Ex ea enim nostrum et officia. Eius non magnam rerum. Et aut accusantium possimus ex officiis voluptas molestiae non Assumenda laudantium quisquam architecto commodi vel nesciunt error ducimus quidem. Deleniti pariatur explicabo in repellendus reprehenderit et. Quidem tempore atque Consequatur exercitationem tempora voluptas quia consequatur voluptatem aut quos velit maiores perferendis aut et sint velit ut voluptatibus voluptatem quisquam voluptatem inventore molestiae dignissimos error impedit et ad asperiores ullam corrupti inventore quas nihil cupiditate fuga rerum quo ipsam illum praesentium quis asperiores et accusamus ut dolorum aut et et aliquid libero laborum nisi qui qui cum enim et illum dolorum consectetur qui totam ut illo qui cupiditate porro laudantium iusto itaque deserunt est autem eius incidunt labore reprehenderit placeat veritatis non eos et quibusdam aliquid voluptate expedita officia modi pariatur dolor quo facilis et ea ut voluptatem est cum distinctio deserunt non non omnis ducimus Ex ea enim nostrum et officia. Eius non magnam rerum. Et aut accusantium possimus ex officiis voluptas molestiae non Assumenda laudantium quisquam architecto commodi vel nesciunt error ducimus quidem. Deleniti pariatur explicabo in repellendus reprehenderit et. Quidem tempore atque Consequatur exercitationem tempora voluptas quia consequatur voluptatem aut quos velit maiores perferendis aut et sint velit ut voluptatibus voluptatem quisquam voluptatem inventore molestiae dignissimos error impedit et ad asperiores ullam corrupti inventore quas nihil cupiditate fuga rerum quo ipsam illum praesentium quis asperiores et accusamus ut dolorum aut et et aliquid libero laborum nisi qui qui cum enim et illum dolorum consectetur qui totam ut illo qui cupiditate porro laudantium iusto itaque deserunt est autem eius incidunt labore reprehenderit placeat veritatis non eos et quibusdam aliquid voluptate expedita officia modi pariatur dolor quo facilis et ea ut voluptatem est cum distinctio deserunt non non omnis ducimus";
-
-let words = paragraph.split(' ');
-
-let sentence = "";
-dataArray = [];
-
-async function sendMultipleMails(mails) {
-  let sendMails = 0;
-  // logic for
-  // sending multiple mails
-  return sendMails;
-}
+let sentence = "",
+  dataArray = {
+    restaurants: [],
+    menus: [],
+    sections: [],
+    items: []
+  },
+  howMany = 0,
+  limit = 10000;
 // receive message from master process
 process.on('message', async (message) => {
-  const numberOfMailsSend = await sendMultipleMails(message.mails);
-
-  fs.writeFile(`./database/datasets/dataset${message.id}.csv`, '', function (err) {
-    if (err) throw err;
-    console.log(`dataset${message.id}.csv Saved!`);
-  });
-  const csvWriter = createCsvWriter({
-    path: `./database/datasets/dataset${message.id}.csv`,
-    header: [
-      { id: 'id', title: 'id' },
-      { id: 'description', title: 'description' },
-    ]
-  });
-
-  console.log(message);
   howMany = message.howMany;
-  let data = {};
-  let randomNum = 0;
-  const sentlength = 25;
-  let sentence = "";
-  startTime = process.hrtime();
-  for (let i = 0; i < howMany; i++) {
-    data = new Object();
-    sentence = "";
-    // randomNum = Math.round(20 * Math.random());
-    // sentence = words.splice(randomNum, (randomNum + sentlength));
-    // sentence.join(',', ' ');
-    for (let j = 0; j < sentlength; j++) {
-      sentence += words[words.length * Math.random() | 0] + ' ';
+  const startID = (howMany * (message.id - 1)) + 1;
+  const endID = howMany * message.id + howMany;
+  console.log('process-message🤘', message);
+
+  if (message.seed === 'restaurants') {
+    let startSeedTime = process.hrtime();
+
+    fs.writeFile(`./database/datasets/restaurants${message.id}.csv`, '', function (err) {
+      if (err) throw err;
+      console.log(`restaurants${message.id}.csv Saved!`);
+    });
+
+    const restaurants = createCsvWriter({
+      path: `./database/datasets/restaurants${message.id}.csv`,
+      header: [
+        { id: 'id', title: 'id' },
+        { id: 'description', title: 'description' },
+        { id: 'title', title: 'title' }
+      ]
+    });
+
+    const menus = createCsvWriter({
+      path: `./database/datasets/menus${message.id}.csv`,
+      header: [
+        { id: 'id', title: 'id' },
+        { id: 'restaurant_id', title: 'restaurant_id' },
+        { id: 'description', title: 'description' },
+        { id: 'title', title: 'title' }
+      ]
+    });
+
+    const sections = createCsvWriter({
+      path: `./database/datasets/sections${message.id}.csv`,
+      header: [
+        { id: 'id', title: 'id' },
+        { id: 'menu_id', title: 'menu_id' },
+        { id: 'description', title: 'description' },
+        { id: 'title', title: 'title' }
+      ]
+    });
+
+
+    const items = createCsvWriter({
+      path: `./database/datasets/items${message.id}.csv`,
+      header: [
+        { id: 'section_id', title: 'section_id' },
+        { id: 'description', title: 'description' },
+        { id: 'title', title: 'title' },
+        { id: 'price', title: 'price' }
+      ]
+    });
+
+    let recordAmt = {
+      restaurants: 0,
+      menus: 0,
+      sections: 0,
+      items: 0,
+    };
+    while (recordAmt.restaurants < howMany) {
+      for (let i = 0; i < limit && recordAmt.restaurants < howMany; i++) {
+
+        dataArray.restaurants.push({
+          id: recordAmt.restaurants + startID, // 8 bytes
+          description: faker.lorem.sentence(), // 2 * length so roughly 200bytes
+          title: faker.lorem.word(), // 2 * length so roughly 50bytes
+        });
+        recordAmt.restaurants += 1;
+
+        // -----------------------------------------------------------
+        // loop to create random amount of menus for current restaurant
+        // ------------------------------------------------------------
+        let menuAmt = Math.round(Math.random() * 3) + 1;
+        for (let j = 0; j < menuAmt; j++) {
+
+          dataArray.menus.push({
+            id: recordAmt.restaurants + startID + recordAmt.menus,
+            restaurant_id: recordAmt.restaurants + startID, // 8 bytes
+            description: faker.lorem.sentence(), // 2 * length so roughly 200bytes
+            title: faker.lorem.word(), // 2 * length so roughly 50bytes
+          })
+          recordAmt.menus += 1;
+          // -----------------------------------------------------------
+          // loop to create random amount of sections for current menu
+          // -----------------------------------------------------------
+          let sectionsAmt = Math.round(Math.random() * 3) + 1;
+          for (let k = 0; k < sectionsAmt; k++) {
+            dataArray.sections.push({
+              id: recordAmt.restaurants + startID + recordAmt.sections,
+              menu_id: recordAmt.restaurants + startID + recordAmt.menus, // 8 bytes
+              description: faker.lorem.sentence(), // 2 * length so roughly 200bytes
+              title: faker.lorem.word(), // 2 * length so roughly 50bytes
+            })
+            recordAmt.sections += 1;
+            // ----------------------------------------------------------------
+            // loop to create random amount of items
+            // ----------------------------------------------------------------
+            let itemsAmt = Math.round(Math.random() * 10) + 1;
+            for (let l = 0; l < itemsAmt; l++) {
+              dataArray.items.push({
+                section_id: recordAmt.restaurants + startID + recordAmt.sections, // 8 bytes
+                description: faker.lorem.sentence(), // 2 * length so roughly 200bytes
+                title: faker.lorem.word(), // 2 * length so roughly 50bytes
+                price: `$${faker.commerce.price()}`,
+              })
+              recordAmt.items += 1;
+            }
+          }
+
+
+          // The limit seems to be around 2M records in the array 512MB / 258 bytes
+          // 2M violates the heap so we just do it nice nice  😎
+        }
+        await restaurants
+          .writeRecords(dataArray.restaurants)
+          .then(() => {
+            // console.log(`The restaurants ${message.id}.csv was written successfully ${process.hrtime(startSeedTime)}`);
+            // clear the dataArray out of RAM / Heap 😎
+            dataArray.restaurants = [];
+          })
+          .catch(err => {
+            //TODO - cleanup the files and delete since we erred
+            process.send({ seed: 'ERROR' })
+            throw err;
+          });
+        await menus
+          .writeRecords(dataArray.menus)
+          .then(() => {
+            // console.log(`The restaurants ${message.id}.csv was written successfully ${process.hrtime(startSeedTime)}`);
+            // clear the dataArray out of RAM / Heap 😎
+            dataArray.menus = [];
+          })
+          .catch(err => {
+            //TODO - cleanup the files and delete since we erred
+            process.send({ seed: 'ERROR' })
+            throw err;
+          });
+        await sections
+          .writeRecords(dataArray.sections)
+          .then(() => {
+            // console.log(`The restaurants ${message.id}.csv was written successfully ${process.hrtime(startSeedTime)}`);
+            // clear the dataArray out of RAM / Heap 😎
+            dataArray.sections = [];
+          })
+          .catch(err => {
+            //TODO - cleanup the files and delete since we erred
+            process.send({ seed: 'ERROR' })
+            throw err;
+          });
+        await items
+          .writeRecords(dataArray.items)
+          .then(() => {
+            // console.log(`The restaurants ${message.id}.csv was written successfully ${process.hrtime(startSeedTime)}`);
+            // clear the dataArray out of RAM / Heap 😎
+            dataArray.items = [];
+          })
+          .catch(err => {
+            //TODO - cleanup the files and delete since we erred
+            process.send({ seed: 'ERROR' })
+            throw err;
+          });
+
+
+      }
+
     }
-    // put the object in the array we will write
-    data.description = sentence;
-    data.id = i;
-    dataArray.push(data);
+    process.send({ seed: 'restaurants', time: process.hrtime(startSeedTime), status: 'done' });
+    console.log(`process_${message.id} made a Total ${recordAmt.restaurants + recordAmt.menus + recordAmt.sections + recordAmt.items} records in ${process.hrtime(startTime)}`);
   }
-  await csvWriter
-    .writeRecords(dataArray)
-    .then(() => {
-      console.log(`The dataset${message.id}.csv file was written successfully `);
-    })
-    .catch(err => { throw err; })
-      process.send({ time: process.hrtime(startTime) });
-      process.exit(0);
+
+  if (message.seed === 'ERROR' || message.seed === 'done') {
+    process.send({ time: process.hrtime(startTime), seed: 'ALL' });
+    process.exit(0);
+  }
 
 });
-
-
--- database/seeder9000/threadedseeder.js
+++ database/seeder9000/threadedseeder.js
@@ -1,29 +1,81 @@
 const { fork } = require('child_process');
 const os = require('os');
-const cpuCount = os.cpus().length;
-let process = {};
+const concat = require('concat');
+const fs = require('fs');
+
+let cpuCount = os.cpus().length;
+let homer = {};
 
 // Consider running processes to available memory
 // https://www.npmjs.com/package/free-memory
 
-// We will only use aprox half of the cpu's but use atleast 1 cpu
-let maxThreads = Math.ceil(cpuCount /2);
+
+//cpuCount = 10; // uncomment to test other cpu amounts
+
+// We will only use aprox half of the cpu's available but use atleast 1 cpu
+let maxThreads = Math.ceil(cpuCount / 1.5);
 howMany = Math.round(10000000 / maxThreads);
+restCSSArray = [];
+menuCSSArray = [];
+itemCSSArray = [];
 
 
-for (let i = 0; i < maxThreads; i++) {
+fs.writeFile(`./database/datasets/restaurants.csv`, '', function (err) {
+  if (err) throw err;
+  console.log(`restaurants.csv Saved!`);
+});
 
-  process[i] = fork('./database/seeder9000/datacreator.js');
-  process[i].send({
-    howMany,
+
+for (let i = 1; i < maxThreads; i++) {
+
+  homer[i] = fork('./database/seeder9000/datacreator.js');
+  homer[i].send({
     id: i,
+    howMany,
+    seed: 'restaurants'
   });
-  process[i].on('message', (message) => {
+  let total = 0;
+  homer[i].on('message', (message) => {
     // Add conditional console logs depending on message.
-    console.log(`Time for process ${i} to make ${howMany} records was ${message.time}`);
+    console.log(`Time for process ${i} to make ${howMany} records was ${message.time} ${message.seed}`);
     // Begin to seed with csv and use appropriate database driver with CLI argument or similar
+    total += message.howMany;
+    if (message.seed === 'restaurants' && message.status === 'done') {
 
+      restCSSArray.push(`./database/datasets/restaurants${i}.csv`);
+      if (restCSSArray.length === maxThreads) {
+        restCSSArray.forEach((filename, i) => {
+          fs.appendFile(`./database/loadPostgres.sql`, `\ncopy "Restaurants".restaurants (id, description, title) FROM '/home/jordan/HackReactor/SDC/menu-service/database/datasets/restaurants${i + 1}.csv' CSV HEADER;\n`, (err) => {
+            if (err){
+              throw err;
+            }
+            fs.appendFile(`./database/loadPostgres.sql`, `\ncopy "Restaurants".menus (id, restaurant_id, description, title) FROM '/home/jordan/HackReactor/SDC/menu-service/database/datasets/menus${i + 1}.csv' CSV HEADER;\n`, (err) => {
+              if (err){
+                throw err;
+              }
+              fs.appendFile(`./database/loadPostgres.sql`, `\ncopy "Restaurants".sections (id, menu_id, description, title) FROM '/home/jordan/HackReactor/SDC/menu-service/database/datasets/sections${i + 1}.csv' CSV HEADER;\n`, (err) => {
+                if (err){
+                  throw err;
+                }
+                fs.appendFile(`./database/loadPostgres.sql`, `\ncopy "Restaurants".items (section_id, description, title) FROM '/home/jordan/HackReactor/SDC/menu-service/database/datasets/items${i + 1}.csv' CSV HEADER;\n`, (err) => {
+                  if (err){
+                    throw err;
+                  }
+
+                })
+              })
+            })
+          })
+
+        })
+      }
+      homer[i].send({ id: i, howMany, seed: 'done' });
+    } else {
+
+      homer[i].send({ id: i, howMany, seed: 'ERROR' });
+
+    }
     // TODO look into compression options for adding and retrieving from database
-    console.log(`TODO - make proc ${i} begin seeding with file dataset${i}.csv`)
+    // console.log(`TODO - make proc ${i} begin seeding with file dataset${i}.csv`)
   });
 }
